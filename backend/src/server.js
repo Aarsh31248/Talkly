54bec25474d73a96f20727d404ffdaf71579e6e5
Aarsh Singh <starfire9599@gmail.com>
2026-05-17 00:07:17 +0530

    Backend Complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,29 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-
 import path from "path";
-
-const app = express();
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+app.set("trust proxy", true);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
@@ -25,6 +31,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
+  connectDB();
 });
